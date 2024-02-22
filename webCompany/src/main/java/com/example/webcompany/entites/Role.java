94ac1e8528649160c11984557161401847bd3bb6
Yanis <mechtayanis20@gmail.com>
2024-02-22 09:35:43 +0100

    modification bd
    Ajout de l'entité role

---
+++ webCompany/src/main/java/com/example/webcompany/entites/Role.java
@@ -0,0 +1,35 @@
+package com.example.webcompany.entites;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Role() {
+
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
